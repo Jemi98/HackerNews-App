b12de9f6d8ffa4479fde4696941d5ac5744da964
Jemi98 <jehangiiir98@gmail.com.com>
2022-05-29 12:48:46 +0500

    3rd commit

--- src/components/FetchNews.js
+++ src/components/FetchNews.js
@@ -9,7 +9,7 @@ const FetchNews = () => {
   const [isLoading, setIsLoading] = useState(true);
 
   useEffect(() => {
-    isLoading(true);
+    setIsLoading(true);
     const fetchNews = async () => {
       const url = `https://hn.algolia.com/api/v1/search?query=${query}`;
       const response = await fetch(url);
@@ -21,7 +21,7 @@ const FetchNews = () => {
       console.log(data);
     };
     fetchNews();
-    isLoading(false);
+    setIsLoading(false);
   }, [query]);
 
   const handleSubmit = (e) => {
@@ -38,31 +38,64 @@ const FetchNews = () => {
   };
 
   return (
-    <div className="container">
-      {/* Search Form*/}
-      <form className="form-inline d-flex justify-content-center">
-        <div className="form-group  m-5 w-50 d-flex">
-          <input
-            type="text"
-            className="form-control"
-            id="input1"
-            placeholder="Enter Something..."
-            required
-            value={text}
-            onChange={(e) => setText(e.target.value)}
-          />
-          <button
-            type="submit"
-            onClick={handleSubmit}
-            className="btn btn-primary m-2 p-3"
-          >
-            Search
-          </button>
-        </div>
-      </form>
+    <>
+      <div className="container">
+        {/* Search Form*/}
+        <form className="form-inline d-flex justify-content-center">
+          <div className="form-group  m-5 w-50 d-flex">
+            <input
+              type="text"
+              className="form-control"
+              id="input1"
+              placeholder="Enter Something..."
+              required
+              value={text}
+              onChange={(e) => setText(e.target.value)}
+            />
+            <button
+              type="submit"
+              onClick={handleSubmit}
+              className="btn btn-primary m-2 p-3"
+            >
+              Search
+            </button>
+          </div>
+        </form>
+      </div>
       {/* End Form*/}
-      <section>Data to be diplay here</section>
-    </div>
+
+      {/* Display data start*/}
+      <section className="d-flex">
+        <div className="row">
+          {item.map((item) => {
+            const { author, created_at, objectID, titile, url } = item;
+
+            return (
+              <div
+                className="card "
+                key={objectID}
+                style={{ width: "300px", height: "250px", margin: "20px" }}
+              >
+                <div className="title">{titile}</div>
+                <div className="card-body">
+                  <h5 className="card-title">Card Title</h5>
+                  <p className="card-text">
+                    <em>By{author}</em>
+                  </p>
+                  <a href={url} className="btn btn-primary">
+                    read more
+                  </a>
+                  <p className="card-text">
+                    <em>Created_at{created_at}</em>
+                  </p>
+                </div>
+              </div>
+            );
+          })}
+        </div>
+      </section>
+      {/* Display data end*/}
+    </>
   );
 };
 
