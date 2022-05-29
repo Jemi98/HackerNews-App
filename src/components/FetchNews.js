037aa519b74a89146328754c29a11b20b0bc8f86
Jemi98 <jehangiiir98@gmail.com.com>
2022-05-29 14:38:01 +0500

    4rth commit

--- src/components/FetchNews.js
+++ src/components/FetchNews.js
@@ -1,11 +1,11 @@
 import "../App.css";
+import { format } from "date-fns";
 import React, { useEffect, useState } from "react";
 
 const FetchNews = () => {
   const [item, setItem] = useState([]);
   const [query, setQuery] = useState("programming");
   const [text, setText] = useState("");
-  const [largeTitle, setLargeTitle] = useState([]);
   const [isLoading, setIsLoading] = useState(true);
 
   useEffect(() => {
@@ -16,7 +16,6 @@ const FetchNews = () => {
       const data = await response.json();
 
       setItem(data.hits);
-      setLargeTitle(data.hits[0]);
 
       console.log(data);
     };
@@ -27,74 +26,101 @@ const FetchNews = () => {
   const handleSubmit = (e) => {
     e.preventDefault();
 
-    if (!text) {
-      console.log("Input is empty");
-    } else {
-      setQuery(text);
-      setText("");
-      console.log(text);
-      console.log(query);
-    }
+    setQuery(text);
+    setText("");
+    console.log(text);
+    console.log(query);
   };
 
   return (
     <>
-      <div className="container">
-        {/* Search Form*/}
-        <form className="form-inline d-flex justify-content-center">
-          <div className="form-group  m-5 w-50 d-flex">
-            <input
-              type="text"
-              className="form-control"
-              id="input1"
-              placeholder="Enter Something..."
-              required
-              value={text}
-              onChange={(e) => setText(e.target.value)}
-            />
-            <button
-              type="submit"
-              onClick={handleSubmit}
-              className="btn btn-primary m-2 p-3"
-            >
-              Search
-            </button>
+      <main>
+        {/* Loading.....  start*/}
+        {isLoading ? (
+          <div class="d-flex justify-content-center">
+            <div class="spinner-border" role="status">
+              <span class="sr-only">Loading...</span>
+            </div>
           </div>
-        </form>
-      </div>
-      {/* End Form*/}
-
-      {/* Display data start*/}
-      <section className="d-flex">
-        <div className="row">
-          {item.map((item) => {
-            const { author, created_at, objectID, titile, url } = item;
-
-            return (
-              <div
-                className="card "
-                key={objectID}
-                style={{ width: "300px", height: "250px", margin: "20px" }}
-              >
-                <div className="title">{titile}</div>
-                <div className="card-body">
-                  <h5 className="card-title">Card Title</h5>
-                  <p className="card-text">
-                    <em>By{author}</em>
-                  </p>
-                  <a href={url} className="btn btn-primary">
-                    read more
-                  </a>
-                  <p className="card-text">
-                    <em>Created_at{created_at}</em>
-                  </p>
+        ) : (
+          <>
+            <div className="main">
+              {/* Search Form*/}
+              <form className="form-inline d-flex justify-content-center">
+                <div className="form-group  m-5 w-50 d-flex">
+                  <input
+                    type="text"
+                    className="form-control"
+                    id="input1"
+                    placeholder="Enter something to search..."
+                    required
+                    value={text}
+                    onChange={(e) => setText(e.target.value)}
+                  />
+                  <button
+                    type="submit"
+                    onClick={handleSubmit}
+                    className="btn btn-primary m-1 p-2 "
+                  >
+                    Search
+                  </button>
                 </div>
+              </form>
+            </div>
+            {/* End Form*/}
+
+            {/* Display data start*/}
+            <section className="bg-light">
+              <div className="row main-row light-gray">
+                {item.map((item) => {
+                  const { author, created_at, objectID, title, url } = item;
+
+                  return (
+                    <div
+                      className="card bg-colour"
+                      key={objectID}
+                      style={{
+                        width: "300px",
+                        height: "250px",
+                        margin: "20px",
+                        radius: "30px",
+                      }}
+                    >
+                      <div className="c">
+                        <div className="title mt-2">
+                          <h5>{title}</h5>
+                        </div>
+                        <p className="card-text">
+                          <em>By-:{author}</em>
+                        </p>
+                        <hr />
+                        <div
+                          className="d-flex justify-content-between"
+                          style={{ marginTop: "70px" }}
+                        >
+                          <a href={url} className="btn btn-secondary ">
+                            read more
+                          </a>
+                          <p className="card-text">
+                            <em>
+                              {format(new Date(created_at), "dd MMM yyyy")}
+                            </em>
+                          </p>
+                        </div>
+                      </div>
+                    </div>
+                  );
+                })}
               </div>
-            );
-          })}
-        </div>
-      </section>
-      {/* Display data end*/}
+            </section>
+
+            <p style={{ textAlign: "center", textTransform: "capitalize" }}>
+              copyright © 2022- Made with 🖤 By Jemi{" "}
+            </p>
+          </>
+        )}
+        {/* Display data end*/}
+      </main>
     </>
   );
 };
